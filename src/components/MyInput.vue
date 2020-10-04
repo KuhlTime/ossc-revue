7454cb3786a5c285929194e6426833ebdd83c2ad
André Kuhlmann <akuhltime@gmail.com>
2020-10-04 21:10:13 +0200

    Add custom input component

---
+++ src/components/MyInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <input
+    type="text"
+    v-model="value"
+    :placeholder="placeholder"
+    :required="required"
+    :name="name"
+    @change="updateValue($event.target.value)"
+    @focus="this.$emit('focus')"
+  />
+</template>
+
+<script>
+// https://paulund.co.uk/use-v-model-on-custom-vue-component
+
+export default {
+  name: 'my-input',
+  props: {
+    type: String,
+    value: String,
+    name: String,
+    placeholder: String,
+    required: Boolean
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  border-radius: 4px;
+  border: 1px solid transparent;
+  color: #fff;
+  font-size: 15px;
+  padding: 6px 12px;
+  background-color: rgba(0, 0, 0, 0.15);
+  transition: all ease-in-out 160ms;
+}
+
+input:focus {
+  outline: none;
+  border: 1px solid rgba(255, 255, 255, 0.9);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,12 @@
             <p class="toolbar-title">{{ tableTitleString }}</p>
           </div>
           <div class="right">
-            <input v-model="search" type="search" name="search" :placeholder="searchPlaceholder" />
+            <MyInput
+              v-model="search"
+              type="search"
+              name="search"
+              :placeholder="searchPlaceholder"
+            />
           </div>
         </div>
       </template>
@@ -20,15 +25,19 @@
 import Container from '@/components/Container'
 import Box from '@/components/Box'
 import DataTable from '@/components/DataTable'
+import MyInput from '@/components/MyInput'
 
 import emptyIcon from '@/assets/icons/empty.svg'
+
+// import exampleData from '@/tmp/sampleData.json'
 
 export default {
   name: 'home',
   components: {
     Container,
     Box,
-    DataTable
+    DataTable,
+    MyInput
   },
   data: () => {
     return {
@@ -69,20 +78,5 @@ export default {
 .toolbar-title {
   font-weight: 500;
   font-size: 17px;
-}
-
-input {
-  border-radius: 4px;
-  border: 1px solid transparent;
-  color: #fff;
-  font-size: 15px;
-  padding: 6px 12px;
-  background-color: rgba(0, 0, 0, 0.15);
-  transition: all ease-in-out 160ms;
-}
-
-input:focus {
-  outline: none;
-  border: 1px solid rgba(255, 255, 255, 0.9);
 }
 </style>
